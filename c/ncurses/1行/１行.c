eafedcc21187a357b2fe918a13364f27accd9316
v02qabs <takesue090@gmail.com>
2025-08-26 16:51:21 +0900

    gtk-Hello-window-x86

--- c/ncurses/1行/１行.c
+++ c/ncurses/1行/１行.c
@@ -1,9 +1,10 @@
 #include <ncurses.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
   char str[80];
-
+  int c;
   initscr();            /* ncursesの初期化 */
   cbreak();             /* バッファリングなしで入力 */
   printw("Enter a line: "); /* プロンプト表示 */
@@ -12,9 +13,16 @@ int main() {
   getstr(str);         /* 1行取得 */
 
   printw("\nYou entered: %s\n", str); /* 入力された文字列を表示 */
-  refresh();            /* 画面更新 */
-  getch();             /* 入力待ち */
-  endwin();             /* ncurses終了 */
+  refresh();  /* 画面更新 */
+  while(true){
+	c = getch();
+	if(c=47){
+		exit(0);
+  	}
+  }
+
+
+  /* 入力待ち */
 
   return 0;
 }
